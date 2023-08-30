703139fbbfe101a050f4a167141a4d1316698871
uneypan <zhihan.scut@gmail.com>
2023-08-30 13:11:09 +0800

    1

--- Practice4/GUI.py
+++ Practice4/GUI.py
@@ -7,37 +7,6 @@ from feature import feature
 
 
 
-def normalize_image(image):
-    # 归一化处理转uint8方便显示
-    normalized_image = (image - np.min(image)) / (np.max(image) - np.min(image)) * 255
-    return normalized_image.astype(np.uint8)
-
-def one_step(image,window):
-    # 一键处理函数
-    normim, mask = ridge_segment(image, blksze = 16, thresh = 0.1)  # normalise the image and find a ROI
-    window['norm'].update(data=cv2.imencode('.png', normalize_image(normim))[1].tobytes())
-    
-    orientim = ridge_orient(im = normim, gradientsigma = 1, blocksigma = 7, orientsmoothsigma = 7)  # find orientation of every pixel
-    color_image = ((orientim / np.pi) * 255).astype(np.uint8)
-    color_image = cv2.applyColorMap(color_image, cv2.COLORMAP_BONE)
-    window['orim'].update(data=cv2.imencode('.png', color_image)[1].tobytes())
-    
-    orientafld = orientation_field(normalize_image(normim),orientim)
-    window['orifld'].update(data=cv2.imencode('.png', orientafld)[1].tobytes())
-    
-    freq, medfreq = ridge_freq(im = normim, mask = mask, orient = orientim, blksze = 12, windsze = 5, minWaveLength = 5,maxWaveLength = 15)  # find the overall frequency of ridges
-    window['freq'].update(data=cv2.imencode('.png', normalize_image(freq))[1].tobytes())
-    
-    newim = ridge_filter(im = normim, orient = orientim, freq = medfreq * mask, kx = 0.5, ky = 0.5)  # create gabor filter and do the actual filtering
-    img = 255 * (newim >= -3)
-    window['enhance'].update(data=cv2.imencode('.png', normalize_image(img))[1].tobytes())
-    
-    thinnim = thinning(img,num=5)
-    window['thin'].update(data=cv2.imencode('.png', normalize_image(thinnim))[1].tobytes())
-    
-    feat = feature(thinnim)
-    window['feat'].update(data=cv2.imencode('.png', normalize_image(feat))[1].tobytes())
-
 def create_gui():
     sg.theme('DefaultNoMoreNagging')
 
@@ -64,7 +33,7 @@ def create_gui():
         if event in (sg.WIN_CLOSED, '退出'):
             break
         elif event == '选择图片':
-            file_path = sg.popup_get_file('选择图片文件', file_types=(("Image Files", "*.png *.jpg *.jpeg *.bmp"),))
+            file_path = sg.popup_get_file('选择图片文件', file_types=(("Image Files", "*.png *.jpg *.jpeg *.bmp *.tif"),))
             if file_path:
                 image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
                 image = cv2.resize(image, (200, 200))
@@ -133,12 +102,68 @@ def create_gui():
                         feat = feature(thinnim)
                         processed_images[index] = normalize_image(feat)
                         window['feat'].update(data=cv2.imencode('.png', processed_images[index])[1].tobytes())
-
-                
-                
-
         
     window.close()
+
+
+def normalize_image(image):
+    # 归一化处理转uint8方便显示
+    normalized_image = (image - np.min(image)) / (np.max(image) - np.min(image)) * 255
+    return normalized_image.astype(np.uint8)
+
+
+def area_filter(image):
+
+    # 反转图像，使指纹为白色，背景为黑色
+    inverted_image = cv2.bitwise_not(image.astype(np.uint8))
+
+    # 连通域标记
+    _, labels, stats, centroids = cv2.connectedComponentsWithStats(inverted_image)
+
+    # 设定阈值来过滤孤立块和孔洞
+    min_area_threshold = 10  # 可根据实际情况调整
+
+    # 创建一个全白图像，将保留的连通域绘制上去
+    filtered_image = np.ones_like(inverted_image) * 255
+
+    for label, stat in enumerate(stats):
+        area = stat[4]  # 连通域的面积
+
+        if area > min_area_threshold:
+            filtered_image[labels == label] = 0
+
+    return cv2.bitwise_not(filtered_image.astype(np.uint8))
+
+def one_step(image,window):
+    # 一键处理函数
+    normim, mask = ridge_segment(image, blksze = 16, thresh = 0.1)  # normalise the image and find a ROI
+    window['norm'].update(data=cv2.imencode('.png', normalize_image(normim))[1].tobytes())
+    
+    orientim = ridge_orient(im = normim, gradientsigma = 1, blocksigma = 7, orientsmoothsigma = 7)  # find orientation of every pixel
+    color_image = ((orientim / np.pi) * 255).astype(np.uint8)
+    color_image = cv2.applyColorMap(color_image, cv2.COLORMAP_BONE)
+    window['orim'].update(data=cv2.imencode('.png', color_image)[1].tobytes())
+    
+    orientafld = orientation_field(normalize_image(normim),orientim)
+    window['orifld'].update(data=cv2.imencode('.png', orientafld)[1].tobytes())
+    
+    freq, medfreq = ridge_freq(im = normim, mask = mask, orient = orientim, blksze = 12, windsze = 5, minWaveLength = 5,maxWaveLength = 15)  # find the overall frequency of ridges
+    window['freq'].update(data=cv2.imencode('.png', normalize_image(freq))[1].tobytes())
+    
+    newim = ridge_filter(im = normim, orient = orientim, freq = medfreq * mask, kx = 0.4, ky = 0.4)  # create gabor filter and do the actual filtering
+    # img = normalize_image(newim)
+    # thresh,img = cv2.threshold(img,200,255,cv2.THRESH_BINARY)
+    img = 255 * (newim >= -3)
+    window['freq'].update(data=cv2.imencode('.png', normalize_image(img))[1].tobytes())
+    img = area_filter(img)
+    window['enhance'].update(data=cv2.imencode('.png', img)[1].tobytes())
+    
+    thinnim = thinning(img,num=10)
+    window['thin'].update(data=cv2.imencode('.png', normalize_image(thinnim))[1].tobytes())
+    
+    feat = feature(thinnim)
+    window['feat'].update(data=cv2.imencode('.png', normalize_image(feat))[1].tobytes())
+
 
 if __name__ == '__main__':
     create_gui()
